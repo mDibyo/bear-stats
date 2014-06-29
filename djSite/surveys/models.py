57670e724d1d8033c2570cc34c6873183cd547ee
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-29 14:58:07 -0700

    Added model for survey

--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    
