056d5dbe048ad6c34a5dd0275e889a68e51d0cec
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-29 22:47:36 -0700

    Added Colleges to surveys model

--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+COLLEGES = (("Chem", "College of Chemistry"),
+            ("CoE", "College of Engineering"),
+            ("CED", "College of Environmental Design"),
+            ("LS", "College of Letters and Sciences"),
+            ("CNR", "College of Natural Resources"),
+            ("Haas", "Haas School of Business"))
+
+MAJORS = (("CHEM", "Chemistry"),
+          ("C"))
+
+
 class Survey(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
