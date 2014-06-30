8f2c0e99589a4e0f2bd5dc5280ef1d504caf9f99
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-29 21:20:36 -0700

    Added model for results

--- djSite/results/models.py
+++ djSite/results/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    username = models.CharField(max_length=20)
+
+class SurveyResult(models.Model):
+    user = models.ForeignKey('User')
+    question = models.ForeignKey('surveys.Question')
+    answer = models.CharField(max_length=100)
