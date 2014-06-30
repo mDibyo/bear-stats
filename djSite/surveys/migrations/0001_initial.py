66bba13735b466cda659b22b3aed60dde6246800
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-29 21:08:36 -0700

    Defined models for surveys

---
+++ djSite/surveys/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('question_text', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Survey',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=500)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Survey(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.TextField()
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    create_date = models.DateField('date created')
     
