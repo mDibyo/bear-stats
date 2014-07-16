a714219535ca82862a331444e430142defe8885d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-15 23:48:02 -0700

    Added concept of files for storing survey questions

--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -15,8 +15,10 @@ MAJORS = (("CHEM", "Chemistry"),
 class Survey(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    questions_file = models.CharField(max_length=100)
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     create_date = models.DateField('date created')
+    reply_type = models.CharField(max_length=10)
