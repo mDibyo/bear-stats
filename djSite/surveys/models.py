d79a2632a1e558407a9d5f039e77ec57f8ec560c
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-16 23:34:34 -0700

    Completed Survey model

--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -15,20 +15,29 @@ MAJORS = (("CHEM", "Chemistry"),
 class Survey(models.Model):
     """
     Each Survey is stored in a separate JSON file with all the questions and 
-    possible answers specified. Example survey JSON file:
+    possible answers specified. This is done so that questions do not have to 
+    be fetched individually from the Questions database when a survey is to be
+    displayed or administered.
+
+    Example survey JSON file:
     {
         "title": "Example Survey",
         "date": "07/16/2014",
-        "context": "This is the reason for creating this survey. This is entered 
-            purely for the sake of documentation. ",
+        "description": "This describes what the survey contains and who is its
+            audience. "
+        "context": "This is the reason for creating this survey. This is
+            entered purely for the sake of documentation. ",
         "questions": [{
-            "question_id": "This is the primary key of the question in the database",
+            "question_id": "This is the primary key of the question in the
+                database",
             "question_text": "What is question number 1?",
+            "create_date": "07/16/2014",
             "reply_type": "How is question number 1 supposed to be answered?",
             "additional_info": {
                 # These are all the additional attributes that are required as
-                # specified by reply_type. Reply_type specifies a set of instructions
-                # on how to parse additional_info as a colon separated list
+                # specified by reply_type. Reply_type specifies a set of
+                # instructions on how to parse additional_info as a colon
+                # separated list
                 "image_file": "/url/to/image/resource",
                 "options": [{
                     "option_text": "Option 1#",
@@ -39,19 +48,24 @@ class Survey(models.Model):
                 }]
             }
         }, {
-            "question_id": "This is the primary key of the question in the database",
+            "question_id": "This is the primary key of the question in the 
+                database",
             "question_text": "What is question number 2?",
+            "create_date": "07/16/2014",
             "reply_type": "How is question number 2 supposed to be answered?",
             "additional_info": {
                 "default_answer": "This is the default answer for question 2"
             }
         }]
     }
-
     """
     title = models.CharField(max_length=100)
     description = models.TextField()
+    context = models.TextField()
     questions_file = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Question(models.Model):
