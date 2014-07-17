349e006a37e2eab849c3dba3be2672b311b5133b
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-16 23:21:32 -0700

    Added to example JSON file in models

--- djSite/surveys/models.py
+++ djSite/surveys/models.py
@@ -22,11 +22,13 @@ class Survey(models.Model):
         "context": "This is the reason for creating this survey. This is entered 
             purely for the sake of documentation. ",
         "questions": [{
+            "question_id": "This is the primary key of the question in the database",
             "question_text": "What is question number 1?",
             "reply_type": "How is question number 1 supposed to be answered?",
-            "additional info": {
-                # This is all the additional attributes that are required as
-                # specified by reply_type
+            "additional_info": {
+                # These are all the additional attributes that are required as
+                # specified by reply_type. Reply_type specifies a set of instructions
+                # on how to parse additional_info as a colon separated list
                 "image_file": "/url/to/image/resource",
                 "options": [{
                     "option_text": "Option 1#",
@@ -35,6 +37,13 @@ class Survey(models.Model):
                     "option_text": "Option #2",
                     "is_selected": false
                 }]
+            }
+        }, {
+            "question_id": "This is the primary key of the question in the database",
+            "question_text": "What is question number 2?",
+            "reply_type": "How is question number 2 supposed to be answered?",
+            "additional_info": {
+                "default_answer": "This is the default answer for question 2"
             }
         }]
     }
